76d7b75cc928cfeece54c73e80738d1f51459276
Teamkung <robotics.za@gmail.com>
2016-12-02 20:33:37 +0700

    update 12/2/2016

--- examples/connectexample.cpp
+++ examples/connectexample.cpp
@@ -1,8 +1,11 @@
-#include "mbed"
+#include "mbed.h"
 #include "Connect.h"
 
-#define MODE_1 1
-#define MODE_2 2
+#define MODE_1 0
+#define MODE_2 1
+
+#define SENSOR_1 0
+#define SENSOR_2 1
 
 Serial serial(D8,D2); //txrx
 
@@ -12,11 +15,18 @@ void onDataRecieve(uint8_t topic, int data){
 
 }
 
+uint8_t* splitInt(uint16_t data){
+    uint8_t buff[2];
+    buff[0] = (data >> 8) & 0xFF;
+    buff[1] = (data & 0xFF);
+    return buff;   
+}
+
 int main(){
-	connect.begin(&serial);
-	connect.setCallBack(onDataRecieve);
-	while(1){
-		connect.writeCommand(MODE_1,500);
-		connect.checkRecieveEvent();
-	}
+    connect.begin(&serial);
+    connect.setCallBack(onDataRecieve);
+    while(1){
+        connect.writeCommand(connect.setMode(MODE_1,SENSOR_1);,splitInt(500),sizeof(uint16_t));
+        connect.checkRecieveEvent();
+    }
 }
